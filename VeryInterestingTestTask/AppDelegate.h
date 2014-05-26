20b0838771e5639115ce30548e7844cd2d23a80a
Valeriy Buev <bva@Mac-Admin.local>
2014-05-26 09:42:43 +0800

    Initial Commit

---
+++ VeryInterestingTestTask/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VeryInterestingTestTask
+//
+//  Created by Valeriy Buev on 26.05.14.
+//  Copyright (c) 2014 bva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VeryInterestingTestTask/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VeryInterestingTestTask
+//
+//  Created by Valeriy Buev on 26.05.14.
+//  Copyright (c) 2014 bva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
